6fdbbd0a6bbfcb0f66f87d090865c2292ba6c2f7
MUHAMMADFARHANHANIF <farhansmit0318@gmail.com>
2024-12-24 00:01:11 +0500

    more

---
+++ learn-sanity/sanity/blog.ts
@@ -0,0 +1,14 @@
+export default {
+    name: 'Blogs',
+    type: 'document',
+    title: 'Blogs',
+    fields: [
+        {
+            title: 'title',
+            type: 'string',
+            title: 'Enter Blog Title'
+        },{
+            
+        }
+
+    ]
